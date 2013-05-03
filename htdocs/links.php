7e85e11a77cc7513cd55d32e12c092d4abe27036
Jan Pollack <pollack.jan@gmail.com>
2013-05-03 12:34:35 +0200

    Revert "Update von Publikationsdaten"
    This reverts commit 8ea18eb243000fb6c49e40f0427fc9ad05ac859b.

--- htdocs/links.php
+++ htdocs/links.php
@@ -20,10 +20,11 @@
 </li> 
 -->
 <li>
-  Gietzelt, Matthias and Goltz, Ursula and Grunwald, Daniel and Lochau,
-  Malte and Marschollek, Michael and Song, Bianying and Wolf, Klaus-Hendrik<br>
-  <em>Arden2ByteCode: A one-pass Arden Syntax compiler for service-oriented
-  decision support systems based on the OSGi platform</em>
+  Gietzelt, M.; Goltz, U.; Grunwald, D.; Lochau, M.; Marschollek, M.;
+  Song, B. &amp; Wolf, K. H.<br>
+  <em>Arden2ByteCode: A one-pass Arden Syntax compiler 
+  for service-oriented decision support systems based on the 
+  OSGi platform</em>
   <a href="http://www.sciencedirect.com/science/article/pii/S0169260711003087">[PDF]</a><br>
   Comput Methods Programs Biomed, 
   Peter L. Reichertz Institute for Medical Informatics, 
