517b672b5d5a96da292ef8216146598ea5273d94
Mike Klimek <m.klimek@tu-bs.de>
2016-10-03 00:04:25 +0200

    Update KnowledgeCategoryTest
    - add compatibility annotations
    - clean up code

--- test/arden/tests/specification/categories/KnowledgeCategoryTest.java
+++ test/arden/tests/specification/categories/KnowledgeCategoryTest.java
@@ -7,106 +7,80 @@ import arden.tests.specification.testcompiler.CompatibilityRule.Compatibility;
 import arden.tests.specification.testcompiler.SpecificationTest;
 
 public class KnowledgeCategoryTest extends SpecificationTest {
-	
+
 	@Test
 	public void testType() throws Exception {
-		String missingSlot = createCodeBuilder().removeSlot("type:").toString();
-		assertInvalid(missingSlot);
-		
-		String type = createCodeBuilder().replaceSlotContent("type:", "data_driven").toString();
-		assertValid(type);
-		
-		String invalid = createCodeBuilder().replaceSlotContent("type:", "experience").toString();
-		assertInvalid(invalid);
+		assertSlotIsRequired("type:");
+
+		/*
+		 * The ArdenCodeBuilder template is automatically set to the correct
+		 * data-driven or data_driven type.
+		 */
+		assertValid(createCodeBuilder().toString());
+
+		assertInvalidSlot("type:", "experience");
 	}
-	
+
 	@Test
-	@Compatibility(max=ArdenVersion.V1)
-	public void testTypeDash() throws Exception {
-		String typeDash = createCodeBuilder()
-				.removeSlot("arden:") // v1
-				.replaceSlotContent("type:", "data-driven").toString();
-		assertValid(typeDash);
+	@Compatibility(max = ArdenVersion.V1, pedantic = true)
+	public void testTypeUnderscoreInvalid() throws Exception {
+		// only valid since version 2
+		assertInvalidSlot("type:", "data_driven");
 	}
-	
+
 	@Test
 	public void testData() throws Exception {
-		String missingSlot = createCodeBuilder().removeSlot("data:").toString();
-		assertInvalid(missingSlot);
-		
-		String empty = createCodeBuilder().clearSlotContent("data:").toString();
-		assertValid(empty);
+		assertSlotIsRequired("data:");
+		assertValidSlot("data:", ""); // empty slot
 	}
 
 	@Test
 	public void testPriority() throws Exception {
-		String missingSlot = createCodeBuilder().removeSlot("priority:").toString();
-		assertValid(missingSlot);
-
-		String priority = createCodeBuilder().replaceSlotContent("priority:", "50").toString();
-		assertValid(priority);
-		
-		String invalid = createCodeBuilder().replaceSlotContent("priority:", "high").toString();
-		assertInvalid(invalid);
-		
-		String invalidRange1 = createCodeBuilder().replaceSlotContent("priority:", "0").toString();
-		assertInvalid(invalidRange1);
-		
-		String invalidRange2 = createCodeBuilder().replaceSlotContent("priority:", "-50").toString();
-		assertInvalid(invalidRange2);
-		
-		String invalidRange3 = createCodeBuilder().replaceSlotContent("priority:", "100").toString();
-		assertInvalid(invalidRange3);
+		assertSlotIsOptional("priority:");
+		assertValidSlot("priority:", "50");
+		assertValidSlot("priority:", "1");
+		assertValidSlot("priority:", "99");
+		assertValidSlot("priority:", "3.141");
+		assertInvalidSlot("priority:", "0");
+		assertInvalidSlot("priority:", "-50");
+		assertInvalidSlot("priority:", "100");
+		assertInvalidSlot("priority:", "");
+		assertInvalidSlot("priority:", "high");
 	}
 
 	@Test
 	public void testEvoke() throws Exception {
-		String missingSlot = createCodeBuilder().removeSlot("evoke:").toString();
-		assertInvalid(missingSlot);
-
-		String empty = createCodeBuilder().clearSlotContent("evoke:").toString();
-		assertValid(empty);
+		assertSlotIsRequired("evoke:");
+		assertValidSlot("evoke:", ""); // empty slot
 	}
 
 	@Test
 	public void testLogic() throws Exception {
-		String missingSlot = createCodeBuilder().removeSlot("logic:").toString();
-		assertInvalid(missingSlot);
-
-		String empty = createCodeBuilder().clearSlotContent("logic:").toString();
-		assertValid(empty);
+		assertSlotIsRequired("logic:");
+		assertValidSlot("logic:", ""); // empty slot
 	}
 
 	@Test
 	public void testAction() throws Exception {
-		String missingSlot = createCodeBuilder().removeSlot("action:").toString();
-		assertInvalid(missingSlot);
-
-		String empty = createCodeBuilder().clearSlotContent("action:").toString();
-		assertValid(empty);
+		assertSlotIsRequired("action:");
+		assertValidSlot("action:", ""); // empty slot
 	}
 
 	@Test
 	public void testUrgency() throws Exception {
-		String missingSlot = createCodeBuilder().removeSlot("urgency:").toString();
-		assertValid(missingSlot);
-		
-		String priority = createCodeBuilder().replaceSlotContent("urgency:", "50").toString();
-		assertValid(priority);
-		
-		String variable = createCodeBuilder()
-				.replaceSlotContent("data:", "urg := 10")
-				.replaceSlotContent("urgency:", "urg").toString();
-		assertValid(variable);
-		
-		String invalidRange1 = createCodeBuilder().replaceSlotContent("urgency:", "0").toString();
-		assertInvalid(invalidRange1);
-		
-		String invalidRange2 = createCodeBuilder().replaceSlotContent("urgency:", "-50").toString();
-		assertInvalid(invalidRange2);
-		
-		String invalidRange3 = createCodeBuilder().replaceSlotContent("urgency:", "100").toString();
-		assertInvalid(invalidRange3);
+		assertSlotIsOptional("urgency:");
+		assertValidSlot("urgency:", "50");
+		assertValidSlot("urgency:", "1");
+		assertValidSlot("urgency:", "99");
+		assertInvalidSlot("urgency:", "0");
+		assertInvalidSlot("urgency:", "-50");
+		assertInvalidSlot("urgency:", "100");
+		assertInvalidSlot("urgency:", "");
 
+		String variable = createCodeBuilder()
+				.addData("urg := 10")
+				.replaceSlotContent("urgency:", "urg")
+				.toString();
+		assertValid(variable);
 	}
 }
