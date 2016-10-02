a860c6fc76d3d2d51650231b6061259039897d30
Mike Klimek <m.klimek@tu-bs.de>
2016-10-03 00:49:47 +0200

    Update WhereOperatorTest
    - clean up code

--- test/arden/tests/specification/operators/WhereOperatorTest.java
+++ test/arden/tests/specification/operators/WhereOperatorTest.java
@@ -14,7 +14,7 @@ public class WhereOperatorTest extends SpecificationTest {
 		assertEvaluatesTo("1 WHERE FALSE", "()");
 		assertEvaluatesTo("(1,2,3) WHERE TRUE", "(1,2,3)");
 		assertEvaluatesTo("1 WHERE (TRUE,FALSE,TRUE)", "(1,1)");
-		
+
 		String data = createCodeBuilder()
 				.addData("x := 5; TIME x := 1990-01-01T00:00:00;")
 				.addData("y := 3; TIME y := 1990-01-02T00:00:00;")
@@ -26,25 +26,23 @@ public class WhereOperatorTest extends SpecificationTest {
 
 	@Test
 	public void testItemCondition() throws Exception {
-		String data = createCodeBuilder()
-				.addData("mylist := (1,2,\"a\");")
-				.toString();
+		String data = createCodeBuilder().addData("mylist := (1,2,\"a\");").toString();
 		assertEvaluatesToWithData(data, "mylist WHERE mylist <= 2", "(1,2)");
-		
+
 		assertEvaluatesTo("(1,2,\"a\") WHERE IT IS NUMBER", "(1,2)");
 		assertEvaluatesTo("(1,2,\"a\") WHERE THEY ARE NUMBER", "(1,2)");
 		assertEvaluatesTo("(1,2,3,4) WHERE (COUNT ((1,2,3,\"x\") WHERE IT IS NUMBER)) <= IT", "(3,4)");
 	}
-	
+
 	@Test
 	public void testInvalidIt() throws Exception {
 		// IT outside of WHERE -> error at compile time or NULL
-		String itOutsideOfWhere = createCodeBuilder().addAction("RETURN IT").toString();
+		String itOutsideOfWhere = createCodeBuilder().addAction("RETURN IT;").toString();
 		try {
 			getCompiler().compile(itOutsideOfWhere);
 			// no error at compile time -> must return NULL
 			assertReturns(itOutsideOfWhere, "NULL");
-		} catch(TestCompilerCompiletimeException e) {
+		} catch (TestCompilerCompiletimeException e) {
 			// error at compile time -> test passed
 		}
 	}
