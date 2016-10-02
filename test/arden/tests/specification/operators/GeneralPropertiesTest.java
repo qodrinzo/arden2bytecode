4d90024d69fece52c87311536aa3fc671d1bc585
Mike Klimek <m.klimek@tu-bs.de>
2016-10-03 00:19:57 +0200

    Update GeneralPropertiesTest
    - add compatibility annotations
    - clean up code
    - new cases:
      - test applicability handling

--- test/arden/tests/specification/operators/GeneralPropertiesTest.java
+++ test/arden/tests/specification/operators/GeneralPropertiesTest.java
@@ -2,9 +2,9 @@ package arden.tests.specification.operators;
 
 import org.junit.Test;
 
-import arden.tests.specification.testcompiler.ArdenCodeBuilder;
+import arden.tests.specification.testcompiler.ArdenVersion;
+import arden.tests.specification.testcompiler.CompatibilityRule.Compatibility;
 import arden.tests.specification.testcompiler.SpecificationTest;
-import arden.tests.specification.testcompiler.TestCompilerException;
 
 public class GeneralPropertiesTest extends SpecificationTest {
 
@@ -19,38 +19,6 @@ public class GeneralPropertiesTest extends SpecificationTest {
 	}
 
 	@Test
-	public void testPrecedence() throws Exception {
-		assertEvaluatesTo("1+2*3", "7");
-		
-		assertEvaluatesTo("LENGTH (TRIM \" x  \")", "1");
-		assertInvalidExpression("LENGTH TRIM \" x  \"");
-		
-		// TODO is this really invalid?
-		// the specifications grammar and precedence table seem to be incompatible
-		//assertInvalidExpression("TIME UPPERCASE \" x  \"");
-		assertEvaluatesTo("TIME (UPPERCASE \" x  \")", "NULL");
-		
-		
-		assertEvaluatesTo("(COUNT (SQRT 5)) DAYS BEFORE 2015-01-05T00:00:00", "2015-01-04T00:00:00");
-		assertEvaluatesTo("COUNT SQRT 5 DAYS BEFORE 2015-01-05T00:00:00", "2015-01-04T00:00:00");
-		
-		assertEvaluatesTo("SUBSTRING (FLOOR (SQRT 5)) CHARACTERS STARTING AT (LENGTH OF \"123\") FROM \"abcdefg\"", "\"cd\"");
-		assertEvaluatesTo("SUBSTRING FLOOR SQRT 5 CHARACTERS STARTING AT LENGTH OF \"123\" FROM \"abcdefg\"", "\"cd\"");
-	}
-
-	@Test
-	public void testAssociativity() throws Exception {
-		// left associative
-		assertEvaluatesTo("3-4-5", "-6");
-		
-		// right associative
-		assertEvaluatesTo("FLOOR ABS SQRT 5", "2");
-		
-		// non associative
-		assertInvalidExpression("2**3**4");
-	}
-
-	@Test
 	public void testPrimaryTimeHandling() throws Exception {
 		String data = createCodeBuilder()
 				.addData("w := 1;")
@@ -58,23 +26,49 @@ public class GeneralPropertiesTest extends SpecificationTest {
 				.addData("y := 3; TIME y := TIME x;")
 				.addData("z := 2; TIME z := 1995-01-01T00:00:00;")
 				.toString();
-		
-		assertVariableReturnsTimeWithData(data, "COS x", "1990-01-01T00:00:00");
-		assertVariableReturnsTimeWithData(data, "x * y", "1990-01-01T00:00:00");
-		assertVariableReturnsTimeWithData(data, "x > y", "1990-01-01T00:00:00");
-		assertVariableReturnsTimeWithData(data, "x * z", "NULL");
-		assertVariableReturnsTimeWithData(data, "y * w", "NULL");
+
+		assertEvaluatesToWithData(data, "TIME COS x", "1990-01-01T00:00:00");
+		assertEvaluatesToWithData(data, "TIME (x * y)", "1990-01-01T00:00:00");
+		assertEvaluatesToWithData(data, "TIME (x > y)", "1990-01-01T00:00:00");
+		assertEvaluatesToWithData(data, "TIME (x * z)", "NULL");
+		assertEvaluatesToWithData(data, "TIME (y * w)", "NULL");
 	}
-	
-	/**
-	 * Saves expression into a variable and returns its time
-	 */
-	private void assertVariableReturnsTimeWithData(String data, String expression, String expectedTime)
-			throws TestCompilerException {
-		String time = new ArdenCodeBuilder(data)
-				.addData("v :=" + expression + ";")
-				.addAction("RETURN TIME v;")
+
+	@Test
+	@Compatibility(min = ArdenVersion.V2_9)
+	public void testApplicabilityHandling() throws Exception {
+		assertEvaluatesTo("APPLICABILITY 1", "TRUE");
+		assertEvaluatesTo("APPLICABILITY COS 1", "TRUE");
+
+		// minimum applicability of parameters
+		String data = createCodeBuilder().addData("x := 1;")
+				.addData("y := 2;")
+				.addData("APPLICABILITY x := TRUTH VALUE 0.5;")
+				.addData("APPLICABILITY y := TRUTH VALUE 0.7;")
 				.toString();
-		assertReturns(time, expectedTime);
+		assertEvaluatesToWithData(data, "APPLICABILITY COS y", "TRUE");
+		assertEvaluatesToWithData(data, "APPLICABILITY (x * y)", "truth value 0.5");
+		assertEvaluatesToWithData(data, "APPLICABILITY (y * x)", "truth value 0.5");
+	}
+
+	@Test
+	public void testPrecedence() throws Exception {
+		assertEvaluatesTo("1+2*3", "7");
+		assertEvaluatesTo("(COUNT (SQRT 5)) DAYS BEFORE 2015-01-05T00:00:00", "2015-01-04T00:00:00");
+		assertEvaluatesTo("COUNT SQRT 5 DAYS BEFORE 2015-01-05T00:00:00", "2015-01-04T00:00:00");
+	}
+
+	@Test
+	public void testAssociativity() throws Exception {
+		// left associative
+		assertEvaluatesTo("3-4-5", "-6");
+
+		// right associative
+		assertEvaluatesTo("ABS SQRT LOG10 10000", "2");
+
+		// non associative
+		assertInvalidExpression("2**3**4");
+		assertEvaluatesTo("(2**3)**4", "4096");
+
 	}
 }
