9fe2d789cc05418a27cf7275f9264e7a2a6cad6f
Mike Klimek <m.klimek@tu-bs.de>
2016-10-02 23:13:53 +0200

    Add new tests to test suite

--- test/arden/tests/specification/SpecificationTestSuite.java
+++ test/arden/tests/specification/SpecificationTestSuite.java
@@ -7,9 +7,11 @@ import org.junit.runners.Suite.SuiteClasses;
 import arden.tests.specification.categories.KnowledgeCategoryTest;
 import arden.tests.specification.categories.LibraryCategoryTest;
 import arden.tests.specification.categories.MaintenanceCategoryTest;
+import arden.tests.specification.categories.ResourcesCategoryTest;
 import arden.tests.specification.operators.AggregationOperatorsTest;
 import arden.tests.specification.operators.ArithmeticOperatorsTest;
 import arden.tests.specification.operators.DurationOperatorsTest;
+import arden.tests.specification.operators.FuzzyOperatorsTest;
 import arden.tests.specification.operators.GeneralPropertiesTest;
 import arden.tests.specification.operators.IsComparisonOperatorsTest;
 import arden.tests.specification.operators.ListOperatorsTest;
@@ -24,6 +26,7 @@ import arden.tests.specification.operators.StringOperatorsTest;
 import arden.tests.specification.operators.TemporalOperatorsTest;
 import arden.tests.specification.operators.TimeFunctionOperatorsTest;
 import arden.tests.specification.operators.TransformationOperatorsTest;
+import arden.tests.specification.operators.TypeConversionOperatorsTest;
 import arden.tests.specification.operators.WhereOperatorTest;
 import arden.tests.specification.structureslots.ActionSlotTest;
 import arden.tests.specification.structureslots.DataSlotTest;
@@ -37,9 +40,11 @@ import arden.tests.specification.structureslots.TokensTest;
 	KnowledgeCategoryTest.class,
 	LibraryCategoryTest.class,
 	MaintenanceCategoryTest.class,
+	ResourcesCategoryTest.class,
 	AggregationOperatorsTest.class,
 	ArithmeticOperatorsTest.class,
 	DurationOperatorsTest.class,
+	FuzzyOperatorsTest.class,
 	GeneralPropertiesTest.class,
 	IsComparisonOperatorsTest.class,
 	ListOperatorsTest.class,
@@ -54,6 +59,7 @@ import arden.tests.specification.structureslots.TokensTest;
 	TemporalOperatorsTest.class,
 	TimeFunctionOperatorsTest.class,
 	TransformationOperatorsTest.class,
+	TypeConversionOperatorsTest.class,
 	WhereOperatorTest.class,
 	ActionSlotTest.class,
 	DataSlotTest.class,
