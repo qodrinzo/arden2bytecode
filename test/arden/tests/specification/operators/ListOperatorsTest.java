de1e63c1a8c7fb76852396bc59e656815b04552b
Mike Klimek <m.klimek@tu-bs.de>
2016-10-03 00:24:57 +0200

    Update ListOperatorsTest
    - add compatibility annotations
    - clean up code
    - new cases:
      - test sort applicability
      - test sort using
      - test add to
      - test remove from

--- test/arden/tests/specification/operators/ListOperatorsTest.java
+++ test/arden/tests/specification/operators/ListOperatorsTest.java
@@ -2,6 +2,8 @@ package arden.tests.specification.operators;
 
 import org.junit.Test;
 
+import arden.tests.specification.testcompiler.ArdenVersion;
+import arden.tests.specification.testcompiler.CompatibilityRule.Compatibility;
 import arden.tests.specification.testcompiler.SpecificationTest;
 
 public class ListOperatorsTest extends SpecificationTest {
@@ -17,7 +19,9 @@ public class ListOperatorsTest extends SpecificationTest {
 
 	@Test
 	public void testConcatenation() throws Exception {
+		assertEvaluatesTo("1,2", "(1,2)");
 		assertEvaluatesTo("(1,2), 3", "(1,2,3)");
+		assertEvaluatesTo("1, (3,4)", "(1,3,4)");
 	}
 
 	@Test
@@ -36,6 +40,7 @@ public class ListOperatorsTest extends SpecificationTest {
 	}
 
 	@Test
+	@Compatibility(min = ArdenVersion.V2)
 	public void testSortData() throws Exception {
 		assertEvaluatesTo("SORT (3,2,1)", "(1,2,3)");
 		assertEvaluatesTo("SORT DATA (3,2,1)", "(1,2,3)");
@@ -45,6 +50,7 @@ public class ListOperatorsTest extends SpecificationTest {
 	}
 
 	@Test
+	@Compatibility(min = ArdenVersion.V2)
 	public void testSortTime() throws Exception {
 		String data = createData();
 		assertEvaluatesToWithData(data, "SORT TIME (y,x)", "(1,2)");
@@ -52,6 +58,60 @@ public class ListOperatorsTest extends SpecificationTest {
 		assertEvaluatesToWithData(data, "SORT TIME (data1, 5)", "NULL");
 		assertEvaluatesToWithData(data, "SORT TIME ()", "()");
 	}
-	
+
+	@Test
+	@Compatibility(min = ArdenVersion.V2_9)
+	public void testSortApplicability() throws Exception {
+		String data = createCodeBuilder()
+				.addData("x := 1; APPLICABILITY x := TRUTH VALUE 0.3;")
+				.addData("y := 2; APPLICABILITY y := TRUTH VALUE 0.9;")
+				.addData("z := 3; APPLICABILITY z := FALSE;")
+				.addData("data2 := (x,y);")
+				.toString();
+		assertEvaluatesToWithData(data, "SORT APPLICABILITY (x,y,z)", "(3,1,2)");
+		assertEvaluatesToWithData(data, "SORT APPLICABILITY (x,y,5)", "(1,2,3)");
+
+		/*
+		 * The example implies that NULL has NULL as applicability, but the
+		 * specification also says "Since null is not allowed as degree of
+		 * applicability, a value is always returned".
+		 */
+		// assertEvaluatesTo("SORT APPLICABILITY (3,1,2,NULL)", "NULL");
+	}
+
+	@Test
+	@Compatibility(min = ArdenVersion.V2_8)
+	public void testSortUsing() throws Exception {
+		String data = createData();
+		assertEvaluatesToWithData(data, "SORT (x,y) USING TIME OF IT", "(1,2)");
+		assertEvaluatesTo("SORT (3,2,1) USING IT", "(1,2,3)");
+		assertEvaluatesTo("SORT (20:00:00,10:00:00,15:00:00) USING EXTRACT HOUR IT", "(10:00:00,15:00:00,20:00:00)");
+	}
+
+	@Test
+	@Compatibility(min = ArdenVersion.V2_8)
+	public void testAddTo() throws Exception {
+		assertEvaluatesTo("ADD 4 TO (1,2,3)", "(1,2,3,4)");
+		assertEvaluatesTo("ADD NULL TO (1,2,3) AT 2", "(1,NULL,2,3)");
+		assertEvaluatesTo("ADD NULL TO (1,2,3) AT 1+1", "(1,NULL,2,3)");
+		assertEvaluatesTo("ADD \"xyz\" TO NULL", "(NULL,\"xyz\")");
+		assertEvaluatesTo("ADD \"xyz\" TO NULL", "(NULL,\"xyz\")");
+		assertEvaluatesTo("ADD 4 TO (1,2,3) AT 5", "(1,2,3,4)");
+		assertEvaluatesTo("ADD 4 TO (1,2,3) AT 0", "(4,1,2,3)");
+		assertEvaluatesTo("ADD 4 TO (1,2,3) AT (-1)", "(4,1,2,3)");
+		assertEvaluatesTo("ADD 4 TO (1,2,3) AT (1,2)", "(4,1,4,2,3)");
+		assertEvaluatesTo("ADD (4,5) TO (1,2,3)", "(1,2,3,4,5)");
+	}
+
+	@Test
+	@Compatibility(min = ArdenVersion.V2_8)
+	public void testRemoveFrom() throws Exception {
+		assertEvaluatesTo("REMOVE 1 FROM (3,2,1)", "(2,1)");
+		assertEvaluatesTo("REMOVE (2,4) FROM (5,4,3,2,1)", "(5,3,1)");
+		assertEvaluatesTo("REMOVE NULL FROM (1,2,3)", "(1,2,3)");
+		assertEvaluatesTo("REMOVE 5 FROM (1,2,3)", "(1,2,3)");
+		assertEvaluatesTo("REMOVE 2 FROM NULL", "(,NULL)");
+		assertEvaluatesTo("REMOVE 1 FROM NULL", "()");
+	}
 
 }
